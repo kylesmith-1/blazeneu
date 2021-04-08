ed08a6963dc6cb6e6032233dd065ab71a10d7434
Kyle Smith <kas.smith2000@gmail.com>
2021-04-08 14:13:27 -0400

    got entries loading on company page

--- server/lib/events_app_web/views/company_view.ex
+++ server/lib/events_app_web/views/company_view.ex
@@ -1,6 +1,7 @@
 defmodule CompanyTestWeb.CompanyView do
   use CompanyTestWeb, :view
   alias CompanyTestWeb.CompanyView
+  alias CompanyTestWeb.EntryView
   alias CompanyTest.Repo
 
   def render("index.json", %{companies: companies}) do
@@ -12,9 +13,22 @@ defmodule CompanyTestWeb.CompanyView do
   end
 
   def render("company.json", %{company: company}) do
+    company = company
+    |> Repo.preload(:entries)
+
+    companyEntries = company.entries
+    |> Repo.preload(:company)
+    |> Repo.preload(:user)
+
+    entries = if Ecto.assoc_loaded?(company.entries) do
+      render_many(companyEntries, EntryView, "entry.json")
+    else
+      nil
+    end
 
     %{id: company.id,
       name: company.name,
-      location: company.location}
+      location: company.location,
+      entries: entries}
   end
 end
