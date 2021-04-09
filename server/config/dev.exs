47cbd7e5a5b17ecef9de2d83c492d2a06d599196
Rebekah Johnson <johnson.re@husky.neu.edu>
2021-04-09 07:45:28 -0400

    homepage styling, just needs company list

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -30,8 +30,8 @@ use Mix.Config
 
 # Configure your database
 config :events_app, CompanyTest.Repo,
-username: "blazeneu",
-password: "atlan99",
+username: "events_app",
+password: "xbk27rr0i",
 database: "events_app_dev",
 hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
