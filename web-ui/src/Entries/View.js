f42a24982b62f101118baf1f5c5e2c004ac6dd56
Kyle Smith <kas.smith2000@gmail.com>
2021-04-09 06:18:34 -0400

    fixed visibly Null co-op cycle issue, could probably be better but it works

--- web-ui/src/Entries/View.js
+++ web-ui/src/Entries/View.js
@@ -17,16 +17,29 @@ import { Row, Col, Container, Button } from 'react-bootstrap';
 
 
 function Entry({entry, company, session}) {
+	function coop_cycle() {
+		if (entry.coop_cycle != null && entry.coop_cycle != "null" && !(entry.coop_cycle.includes("Null"))) {
+			return (<h6>Co-op Cycle? {entry.coop_cycle}</h6>);
+		}
+	}
+
+	function additional_notes() {
+		if (entry.additional_notes != null && entry.additional_notes != "null" && entry.additional_notes != "") {
+			return (<h6>Additional Notes? {entry.additional_notes}</h6>);
+		}
+	}
+
+
 	return(
 		<Container className="company-details">
 				<Row>
 					<Col>
 						{/* <h5>{entry.user.email}</h5> */}
 						<h5>This is an entry {entry.id} </h5>
-						<h6>Co-op Cycle? {entry.coop_cycle}</h6>
+						{coop_cycle()}
 						<h6>Drug testing? {entry.drug_test.toString()}</h6>
 						<h6>Continious Drug testing? {entry.continuous_drug_testing.toString()}</h6>
-						<h6>Additional Notes? {entry.additional_notes}</h6>
+						{additional_notes()}
 						<br />
 					</Col>
 				</Row>
