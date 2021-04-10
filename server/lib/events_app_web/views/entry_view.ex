b7c39f9a4a2867e4489695d91343f05bbcfd3ea1
blaze <blaze@guest.guest>
2021-04-10 20:57:26 +0000

    preloading in entry_view

--- server/lib/events_app_web/views/entry_view.ex
+++ server/lib/events_app_web/views/entry_view.ex
@@ -7,16 +7,26 @@ defmodule CompanyTestWeb.EntryView do
   end
 
   def render("show.json", %{entry: entry}) do
+    entry = entry
+    |> Repo.preload(:company)
+    |> Repo.preload(:user)
+
     %{data: render_one(entry, EntryView, "entry.json")}
   end
 
   def render("entry.json", %{entry: entry}) do
+   user = if Ecto.assoc_loaded?(entry.user) do
+      render_one(entry.user, UserView, "user.json")
+    else
+      nil
+    end
     %{id: entry.id,
       drug_test: entry.drug_test,
       continuous_drug_testing: entry.continuous_drug_testing,
       coop_cycle: entry.coop_cycle,
       additional_notes: entry.additional_notes,
       verified: entry.verified,
-      user_id: entry.user_id}
+      user_id: entry.user_id,
+      company_id: entry.company_id}
   end
 end
